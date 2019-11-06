1a3793427f34c5fb1fc385561aa491240c3880b7
Nullpointer <nullpointerworks@outlook.com>
2019-11-06 16:42:31 +0100

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -2,14 +2,13 @@
  * Creative Commons - Attribution, Share Alike 4.0<br>
  * Nullpointer Works (2019)<br>
  * Use of this library is subject to license terms.<br>
- * @version 0.7.1 experimental pre-release
+ * @version 0.8.0 experimental pre-release
  * @author Michiel Drost - Nullpointer Works
  */
 module libnpw.http
 {
 	requires transitive libnpw.util;
 	exports exp.nullpointerworks.http;
-	exports exp.nullpointerworks.http.doc;
 	exports exp.nullpointerworks.http.encoding;
 	exports exp.nullpointerworks.http.header;
 	exports exp.nullpointerworks.http.types;
