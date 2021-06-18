9a37610b4b6d2fade9626a87ae490b8eb573c4d1
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-06-18 13:23:12 +0200

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,7 @@
  * (http://unlicense.org/)
  * Nullpointer Works (2021)
  *
- * @version 0.16.1 experimental
+ * @version 0.16.2 experimental
  * @author Michiel Drost - Nullpointer Works
  */
 module libnpw.http
